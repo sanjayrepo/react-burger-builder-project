551b754ee3d150ce3d282205caae26b0b3d9fc49
sanjay_pradhan <sanjay.p@nooor.in>
2020-04-21 07:36:30 +0530

    removing ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,9 +2,14 @@ import React from 'react';
 import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Lable}>{props.lable}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More} onClick={props.added}> More</button>
+        <div className={classes.Lable} >{props.lable}</div>
+        <button
+            className={classes.Less}
+            onClick={props.removed}
+            disabled={props.disabled}>Less</button>
+        <button
+            className={classes.More}
+            onClick={props.added}> More</button>
     </div >
 );
 
